f8c4e4dd14dfc502b89aeef8c24a51c816649f07
Hieu Nguyen <HieuJames204@gmail.com>
2024-11-11 11:51:06 +0700

    Add some models

---
+++ src/main/java/algo/Sorting.java
@@ -0,0 +1,50 @@
+package algo;
+
+import data_structures.List;
+
+public class Sorting {
+    private static <T extends Comparable<T>> void swap(List<T> list, int i, int j) {
+        T temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    private static <T extends Comparable<T>> int medianOfTheThree(List<T> list, int left, int right) {
+        int mid = (left + right) / 2;
+        T a = list.get(left);
+        T b = list.get(mid);
+        T c = list.get(right);
+
+        if ((b.compareTo(a) <= 0 && a.compareTo(c) <= 0) || (c.compareTo(a) <= 0 && a.compareTo(b) <= 0)) {
+            return left;
+        } else if ((a.compareTo(b) <= 0 && b.compareTo(c) <= 0) || (c.compareTo(b) <= 0 && b.compareTo(a) <= 0)) {
+            return mid;
+        } else {
+            return right;
+        }
+    }
+
+    public static <T extends Comparable<T>> void quickSort(List<T> list, int left, int right) {
+        if (left < right) {
+            int partitionPos = partition(list, left, right);
+            quickSort(list, left, partitionPos - 1);
+            quickSort(list, partitionPos + 1, right);
+        }
+    }
+
+    private static <T extends Comparable<T>> int partition(List<T> list, int left, int right) {
+        int pivotIndex = medianOfTheThree(list, left, right);
+        T pivot = list.get(pivotIndex);
+        swap(list, pivotIndex, right);
+
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (list.get(j).compareTo(pivot) < 0) {
+                swap(list, ++i, j);
+            }
+        }
+
+        swap(list, i + 1, right);
+        return i + 1;
+    }
+}
---
+++ src/main/java/data_structures/Queue.java
@@ -0,0 +1,88 @@
+package data_structures;
+
+public class Queue<T> {
+    private static class Item<T> {
+        private T value;
+        private Item<T> next;
+
+        public Item(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Item<T> head;
+    private int size;
+
+    public Queue() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void offer(T value) {
+        Item<T> newItem = new Item<>(value);
+
+        if (this.isEmpty()) {
+            this.head = newItem;
+        } else {
+            Item<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newItem;
+        }
+
+        size++;
+    }
+
+    public T poll() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty.");
+        }
+
+        T polledItem = head.value;
+
+        Item<T> temp = head;
+        head = head.next;
+        temp.next = null;
+        size--;
+
+        return polledItem;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty.");
+        }
+
+        return head.value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+
+        Item<T> temp = head;
+
+        while (temp != null) {
+            builder.append(temp.value);
+
+            if (temp.next != null) {
+                builder.append(", ");
+            }
+
+            temp = temp.next;
+        }
+
+        builder.append("]");
+        return builder.toString();
+    }
+}
